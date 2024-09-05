8651df9aad0dc699a469a4229f48bead94771431
Shweta Mathanker <135358941+Shweta-Mathanker@users.noreply.github.com>
2024-09-05 19:20:14 +0530

    Add files via upload

---
+++ 6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n,m;
+    printf("give two values:");
+    scanf("%i %i",&n,&m);
+    if(n==m){
+        printf("equal");
+    }
+    else{
+        printf("not equal");
+    }
+}
